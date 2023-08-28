5a9e06e657d86c8fa29c3e3d325bb93b4bf2a1fc
Indo Krish <55687559+Indok93@users.noreply.github.com>
2023-08-28 11:23:50 +0200

    første sæt af js opgaver
    variabler

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+/* 1 Opret en variabel og tildel den en værdi:
+Opret en variabel kaldet navn og tildel den dit eget navn.
+Derefter skal du bruge console.log til at udskrive værdien af variablen navn til konsollen. */
+
+const navn = 'Indo';
+
+console.log(navn);
+
+
+/* 2 Beregning med variabler:
+Opret to variabler, tal1 og tal2, og tildel dem to forskellige numeriske værdier.
+Lav derefter en variabel kaldet sum og tildel den summen af tal1 og tal2. Brug console.log til at udskrive værdien af sum til konsollen. */
+
+let tal1 = 7;
+let tal2 = 11;
+
+const sum = tal1 + tal2;
+
+console.log(sum);
+
+
+/* 3 Ændre værdien af en variabel:
+Opret en variabel kaldet alder og tildel den din alder.
+Derefter skal du ændre værdien af alder til at være 5 år ældre og udskrive den nye værdi til konsollen. */
+
+let alder =  29;
+alder += 5;
+
+console.log(alder);
+
+/* 4 Increment og Decrement variabler
+opret en variabel kaldet myNumber med tallet 1. */
+
+let myNumber = 1;
+
+/* increment myNumber med 1.
+udskriv den nye værdi til konsollen. */
+myNumber ++;
+console.log(myNumber);
+
+/* decrement myNumber med 1.
+ udskriv den nye værdi til konsollen. */
+myNumber --;
+ console.log(myNumber);
